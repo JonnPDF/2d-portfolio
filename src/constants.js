08fb55299ce378fcbe2332a7b2bafe64fd014513
Youssef Shehata <87268040+JonnPDF@users.noreply.github.com>
2025-03-01 06:42:47 +0200

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in python these days; I'm learning AI and ML.
+    I've made a couple of projects in that language. I also like to code some projects using Java, JavaScript and many others.
+    Here is my <a href="https://github.com/JonnPDF" target="_blank">Github</a> and my <a href="https://linkedin.com/in/youssef-shehata-8095231bb" target="_blank">LinkedIn</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of my achievemnts: Graduating with GPA 3.94 and ranking 2nd in my class.
+  I also won the best project award for many of my projects and proud of my last one. It was a great experience!`,
+  "ibm-cert": `This is my <a href="https://github.com/JonnPDF/JonnPDF/blob/main/IBMAiSpecialization.pdf" target="_blank">IBM AI Specialization certification</a>. I took it on Coursera. It was a great course! I am proud of holding it.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube about tech. 
+  I also like to watch about different, and maybe random, subjects like human biology, quantum physics, mathematics, history, chess, and cars. I'm a curious person!`,
+  tv: `That's my TV. I've been watching tech/mathematics youtubers/courses a lot recently like :
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org</a>, <a href="https://www.youtube.com/@CodeBullet" target="_blank">CodeBullet</a>,
+  <a href="https://www.youtube.com/@3blue1brown" target="_blank">3Blue1Brown</a> (one of my favs) and <a href="https://www.youtube.com/@stanfordonline" target="_blank">Stanford Online</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed (maybe). When an idea strikes, I often get up and start doing something about it, even if it's 3 AM! Most of the time I can't help it!`,
+  resume: `This is my desk and I have my resume on it. <a href="https://github.com/JonnPDF/JonnPDF/blob/main/MyCV.pdf" target="_blank">Check it out?</a>
+  Contact me at <a href="mailto:youssefmis2002@hotmail.com" target="_blank">youssefmis2002@hotmail.com</a> if you have anything interesting, and I'd be happy to reply!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my other projects <a href="https://linkedin.com/in/youssef-shehata-8095231bb" target="_blank">in the projects section</a>.`,
+  library: `There are some programming books on my shelves. There is one for Machine Learning by Andrew NG. And another one called Probability and Statistics!
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them? (You'd be surprised by the answer)`,
+  exit: `You want to leave? I understand. Thanks for visiting! Just close the tab ;)`
+}
